6e5db57997d9b837d43cf77b5f57161d865114c5
David Bakhalbashian <davidbakhalbashian@MacBook-Pro-David.local>
2023-10-29 18:04:38 +0400

    create-files

---
+++ src/pages/Registr/Registr.js
@@ -0,0 +1,15 @@
+import './Registr.css'
+
+const Registr = ()=>{
+    return(
+        <div className="Registr">
+       <div className="Module">
+        <div>10</div>
+        <div>10</div>
+        <div>1-</div>
+       </div>
+        </div>
+    )
+}
+
+export default Registr
