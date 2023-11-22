0dff3317c79265e9e97b0c899c290f69c4ba26af
David <davit.bakhalbashian@gmail.com>
2023-11-22 19:30:02 +0400

    create lost page

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Registr from "../pages/Registr/Registr";
 import Game from "../pages/Game/Game";
+import Lost from "../pages/Lost/Lost";
 const Navigation =()=>{
     return(
 <Routes>
 <Route path="/" element={<Registr />}/>
 <Route path="/game" element={<Game />}/>
+<Route path="/lost" element={<Lost />}/>
 </Routes>
     )
 }
